b0a291c67299337165c2dd6f79e9722de228345b
staceylee2002 <stacey.lee@mastercard.com>
2025-02-20 19:08:15 +0000

    challenge 1-5

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,12 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+SELECT * FROM Dishes ORDER BY Price DESC;
 -- Create a report showing appetizers and beverages.
+SELECT * FROM Dishes 
+WHERE Type='Appetizer' OR Type='Beverage'
+ORDER BY Type;
 -- Create a report with all items except beverages.
+SELECT * FROM Dishes 
+WHERE Type <> 'Beverage'
+ORDER BY Type;
