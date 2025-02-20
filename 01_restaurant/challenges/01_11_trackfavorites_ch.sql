28b5f57059dc14d40b2d130226979b303212f726
staceylee2002 <stacey.lee@mastercard.com>
2025-02-20 21:08:35 +0000

    challenge 11-12

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,12 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+SELECT * FROM Customers WHERE FirstName = 'Cleo';
+
+UPDATE Customers 
+SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad')
+WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE FirstName = 'Cleo');
+-- If(@@ROWCOUNT=0)
+-- INSERT INTO CustomerDishes (CustomerID, DishID)
+-- VALUES ((SELECT CustomerID FROM Customers WHERE FirstName = 'Cleo'), (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad'));
+
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,8 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT Customers.FirstName, Customers.LastName, Customers.Email, COUNT(Orders.orderID) as OrderCount
+FROM Customers INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Customers.CustomerID
+ORDER BY OrderCount DESC;
